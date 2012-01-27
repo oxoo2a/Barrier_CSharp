0fb81795cbfd4f2d721bb62abbf5609589ea8602
Peter Sturm <sturm@uni-trier.de>
2012-01-27 11:30:42 +0100

    Initial commit.

---
+++ Barrier Synchronization/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Barrier_Synchronization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
